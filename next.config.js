3f3d3944b87b07cb4358a7b3bc4bb8cdd8dd043d
Davidzzzzhu <david@heyrevia.ai>
2025-11-19 14:55:32 -0800

    revert next.config.js

--- next.config.js
+++ next.config.js
@@ -5,17 +5,12 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             value:
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.connect.aws *.cloudfront.net https://openloop.my.connect.aws; " +
-              "connect-src 'self' *.connect.aws *.cloudfront.net https://openloop.my.connect.aws wss://*.connect.aws; " +
-              "img-src 'self' data: https: *.connect.aws *.cloudfront.net; " +
-              "frame-src 'self' *.connect.aws *.cloudfront.net https://openloop.my.connect.aws; " +
-              "style-src 'self' 'unsafe-inline' *.connect.aws *.cloudfront.net; " +
-              "font-src 'self' data: *.connect.aws *.cloudfront.net;",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.connect.aws *.cloudfront.net;",
           },
         ],
       },
@@ -24,5 +19,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-
